d64a0387bdcce3370974598a12e6dee856992d85
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2022-04-22 18:02:55 -0700

    Add app bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import AppBar from "@mui/material/AppBar";
+import Stack from "@mui/material/Stack";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Monster from "./components/Monster";
+
+function appBarLabel(label) {
+	return (
+		<Toolbar>
+			<IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+				<MenuIcon />
+			</IconButton>
+			<Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+				{label}
+			</Typography>
+		</Toolbar>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<AppBar enableColorOnDark>{appBarLabel("Pokedex")}</AppBar>
+			{/* Blank toolbar: see MUI docs, bumps page content out from behind app bar. */}
+			<Toolbar />
+			<Monster />
+			<Monster />
+			<Monster />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Monster.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+function Monster() {
+	return (
+		<div>
+			<Typography variant="h4">Monster</Typography>
+			<Typography variant="h5">Type:</Typography>
+		</div>
+	);
+}
+
+export default Monster;
